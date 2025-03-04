6cc4f7f80c329c8311bed389f1f6a0d4aad59c9e
Samir Padekar <samirpadekar95@gmail.com>
2025-03-04 12:36:17 +0530

    Update database.py
    add Database password variables

--- database.py
+++ database.py
@@ -7,8 +7,7 @@ import psycopg
 
 load_dotenv(os.path.join('TRIAL_JWT', '.env'))
 
-# SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL_PG")
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg://samirpadekar:Honcho2025@localhost/honcho"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL_PG")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
